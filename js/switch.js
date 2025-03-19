195da5c9893e3d437ec80545fcd55849d67a6b7b
badida2205 <andriaferadze398@gmail.com>
2025-03-19 21:40:39 +0400

    vsio

--- js/switch.js
+++ js/switch.js
@@ -3,21 +3,23 @@ function switchf(p){
 console.log(p.innerHTML);
 if(p.innerHTML == '<i class="fa-solid fa-toggle-off" aria-hidden="true"></i>'){
     p.innerHTML = '<i class="fa-solid fa-toggle-on"></i>'
-     document.documentElement.style.setProperty("--blue","#2c3e50");
-      document.documentElement.style.setProperty("--green","#4ca1af");
-      document.documentElement.style.setProperty("--wight","#2c5364");
-      document.documentElement.style.setProperty("--brown","#abbaab");
-      document.documentElement.style.setProperty("--black","#ffffff");
+     document.documentElement.style.setProperty("--blue","#0d2b1d");
+      document.documentElement.style.setProperty("--green","#6b8f71");
+      document.documentElement.style.setProperty("--wight","#ffffff");
+      document.documentElement.style.setProperty("--brown","#a6a6a6");
+      document.documentElement.style.setProperty("--black","#d0d0d0");
+      document.documentElement.style.setProperty("--red","#e3efd3");
       document.documentElement.style.setProperty("--gamwirvale","rgba(0,0,0,0.6)");
       document.documentElement.style.setProperty("--darktextcolor","#ffffff");
 }
 else{
     p.innerHTML = '<i class="fa-solid fa-toggle-off" aria-hidden="true"></i>';
-    document.documentElement.style.setProperty("--blue","#abbaab");
-    document.documentElement.style.setProperty("--brown","#2c3e50");
-      document.documentElement.style.setProperty("--black","#4ca1af");
-    document.documentElement.style.setProperty("--green","#ffffff");
-    document.documentElement.style.setProperty("--wight","#FFFFFF");
+    document.documentElement.style.setProperty("--blue","#a6a6a6"); 
+       document.documentElement.style.setProperty("--green","#d0d0d0");
+      document.documentElement.style.setProperty("--wight","#FFFFFF");
+    document.documentElement.style.setProperty("--brown","#0d2b1d");
+      document.documentElement.style.setProperty("--black","#e3efd3");
+document.documentElement.style.setProperty("--red","#6b8f71");
     document.documentElement.style.setProperty("--gamwirvale","rgba(255,255,255,0.6)");
     document.documentElement.style.setProperty("--darktextcolor","#000000");
 }
